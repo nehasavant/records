ce0965539f3415a6605b06c854fc8173dbccdd8c
Neha Savant <ngs2116@columbia.edu>
2018-03-31 14:57:40 -0400

    updated setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
+
+
+# parse version string from the __init__ file
+with open("records/__init__.py", "r") as initfile:
+    lines = initfile.readlines()
+    for line in lines:
+        if "__version__" in line:
+            # get version line and strip white space and quotations
+            version = line.strip().split()[-1].strip("'").strip('"')
+
+
+# build command
 setup(
     name="records",
-    version="0.1",
+    version=version,
     packages=find_packages(),
     author="Neha Savant",
+    author_email="ngs2116@columbia.edu",
     license="GPLv3",
-    description="A package for simulating sequences & calculating statistics",
+    description="A package for querying GBIF",
     classifiers=["Programming Language :: Python :: 3"],
 )
