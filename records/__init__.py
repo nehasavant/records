9fd9b9ed4d4661c838a6dc9af00e761d46f672c1
Neha Savant <ngs2116@columbia.edu>
2018-03-31 14:59:08 -0400

    updated init file

--- records/__init__.py
+++ records/__init__.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
 
-from records.records import Records
+__version__ = "0.2"
+
+from .records import Records
+from .records import Epochs
+from .records import load_epochs_from_csv
